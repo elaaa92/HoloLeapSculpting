046c35415b4fe9c490c68fcae49bf6dcb10b49e5
elaaa92 <eladomus07@gmail.com>
2018-11-30 11:25:16 +0100

    Holotoolkit set

---
+++ Assets/Scripts/ButtonsBehavior.cs
@@ -0,0 +1,41 @@
+﻿using HoloToolkit.Unity;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.Receivers;
+using HoloToolkit.Unity.InputModule;
+
+public class ButtonsBehavior : InteractionReceiver
+{
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    protected override void InputDown(GameObject obj, InputEventData eventData)
+    {
+        Debug.Log(obj.name + " : InputDown");
+        GestureAction gestureAction = gameObject.GetComponentInParent<GestureAction>();
+
+        switch (obj.name)
+        {
+            case "MoveButton":
+                gestureAction.SetMode(0);
+                break;
+
+            case "RotateButton":
+                gestureAction.SetMode(1);
+                break;
+
+            case "ResizeButton":
+                gestureAction.SetMode(2);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    protected override void InputUp(GameObject obj, InputEventData eventData)
+    {
+    }
+}
